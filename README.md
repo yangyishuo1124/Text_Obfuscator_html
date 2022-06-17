cc64dd7cab808a98bd07808d1037601391c51618
yangyishuo1124 <2865172449aaazzz@sina.com>
2022-06-17 11:15:59 +0800

    v1.1.1-修复一个溢出bug

--- README.md
+++ README.md
@@ -1,18 +1,21 @@
-# 文本混淆器_html版 V1.1.0-brython版
+# 文本混淆器_html版 V1.1.1-brython版
 ## 功能
 - 将文本混淆为不可读文本（指伪乱码)
 ## 使用方法
     1.下载最新Releases，解压
-    2.打开index.html
+    2.打开index.html或index_onlyone.html
     3.按照提示输入所需文本
     4.点击“加密”或“解密”按钮，获得加密或解密后的文本
+Tip:‘index.html’需要浏览器开启axel（好像是这个词），而使用‘index_onlyone.html’不需要额外支持，即开即用，建议个人用‘index.html’，服务器网站用‘index_onlyone.html’
 ## 用途
     1.混淆网站链接（某度云盘链接等），防止被软件和谐
     2.密文传递，保护隐私
 ## 原理
-    通过将密钥的ascii码（或数字之和）与文本的ascii码相加，得到新的ascii码，
+    通过将密钥的ascii码之和（或数字之和）与文本的ascii码相加，得到新的ascii码，
     再转换为字符，得到新的文本。
 ##更新日志
+    v1.1.1
+    修复一个溢出bug
     v1.1.0
     解决的v1.0.0的无法部署问题，改用brython，可在网站部署
     v1.0.0
--- main.py
+++ main.py
@@ -1,81 +1,87 @@
-class 加密(object):
-    def __init__(self, 密钥: str, 内容: str):
+class jiami(object):
+    def __init__(self, key: str, data: str):
         # 密钥和内容长度
-        self.密钥 = 密钥
-        self.密钥和 = 0
-        if self.密钥.isdigit():
-            if len(self.密钥) > 5:
-                for i in self.密钥:
-                    self.密钥和 += int(i)
+        self.key = key
+        self.key_add = 0
+        if self.key.isdigit():
+            if len(self.key) > 5:
+                for i in self.key:
+                    self.key_add += int(i)
             else:
-                self.密钥和 = int(self.密钥)
+                self.key_add = int(self.key)
         else:
-            for i in self.密钥:
-                self.密钥和 += int(ord(i))
-        self.内容 = 内容
-        self.加密结果列表 = []
-        self.加密结果 = ''
+            for i in self.key:
+                self.key_add += int(ord(i))
+        self.data = data
+        self.jiami_result_list = []
+        self.jiami_result = ''
 
-    def 加密(self):
-        for i in self.内容:
-            self.加密结果列表.append(chr(ord(i) + self.密钥和))
-        for i in self.加密结果列表:
-            self.加密结果 += i
+    def jiami(self):
+        for i in self.data:
+            self.jiami_result_list.append(chr(ord(i) + self.key_add))
+        for i in self.jiami_result_list:
+            self.jiami_result += i
 
-    def 输出(self):
-        return self.加密结果
+    def result(self):
+        return self.jiami_result
 
 
-class 解密(object):
-    def __init__(self,密钥: str,密文: str):
-        self.密钥 = 密钥
-        self.密钥和= 0
-        if self.密钥.isdigit():
-            if len(self.密钥) > 5:
-                for i in self.密钥:
-                    self.密钥和 += int(i)
+class jiemi(object):
+    def __init__(self,key: str,data: str):
+        self.key = key
+        self.key_add= 0
+        if self.key.isdigit():
+            if len(self.key) > 5:
+                for i in self.key:
+                    self.key_add += int(i)
             else:
-                self.密钥和 = int(self.密钥)
+                self.key_add = int(self.key)
         else:
-            for i in self.密钥:
-                self.密钥和 += int(ord(i))
-        self.密文 = 密文
-        self.解密结果列表 = []
-        self.密文结果 = ''
-    def 解密(self):
-        for i in self.密文:
-            self.解密结果列表.append(chr(ord(i) - self.密钥和))
-        for i in self.解密结果列表:
-            self.密文结果 += i
+            for i in self.key:
+                self.key_add += int(ord(i))
+        self.data = data
+        self.jiemi_result_list = []
+        self.jiemi_result = ''
+    def jiemi(self):
+        for i in self.data:
+            try:
+                self.jiemi_result_list.append(chr(ord(i) - self.key_add))
+            except ValueError:
+                self.jiemi_result = '输入的内容不是密文'
+                break
+        for i in self.jiemi_result_list:
+            self.jiemi_result += i
 
-    def 输出(self):
-        return self.密文结果
+    def result(self):
+        return self.jiemi_result
 
 
 
-def jiamimain(密钥, 内容):
-    # 加密
-    if len(密钥) >= 8:
+def jiamimain(key, data):
+    # jiami
+    if len(key) >= 8:
         return '密钥长度错误,请重新输入(密钥长度不能大于8)'
-    elif len(内容) == 0:
+    elif len(data) == 0:
         return '内容不能为空'
-    elif len(密钥) == 0:
+    elif len(key) == 0:
         return '密钥不能为空'
     else:
-        a = 加密(密钥, 内容)
-        a.加密()
-        return a.输出()
+        a = jiami(key, data)
+        a.jiami()
+        return a.result()
 
 
-def jiemimain(密钥, 内容):
-    # 解密
-    if len(密钥) >= 8:
+def jiemimain(key, data):
+    # jiemi
+    if len(key) >= 8:
         return '密钥长度错误,请重新输入(密钥长度不能大于8)'
-    elif len(内容) == 0:
+    elif len(data) == 0:
         return '内容不能为空'
-    elif len(密钥) == 0:
+    elif len(key) == 0:
         return '密钥不能为空'
     else:
-        a = 解密(密钥, 内容)
-        a.解密()
-        return a.输出()
+        a = jiemi(key, data)
+        a.jiemi()
+        return a.result()
+
+
